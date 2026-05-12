30b5871da4b810723e8543734f82e148476626a4
Prasadkhose09 <Prasadkhose512@gmail.com>
2026-05-12 18:24:20 +0530

    Completed- Stack Implementation Using Array and Linked List

---
+++ Stack/LinkedListStack.java
@@ -0,0 +1,65 @@
+package Stack;
+
+public class LinkedListStack {
+
+    private static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    Node top = null;
+    int size = 0;
+
+    int size() {
+        return size;
+    }
+
+    boolean isEmpty() {
+        return size == 0;
+    }
+
+    // Peek
+    int peek() {
+        return top.data;
+    }
+
+    // Push
+    void push(int data) {
+        Node node = new Node(data);
+        node.next = top;
+        top = node;
+        size++;
+    }
+
+    // Pop
+    int pop() {
+        if (isEmpty())
+            throw new RuntimeException("Empty!");
+
+        int val = top.data;
+        top = top.next;
+        size--;
+        return val;
+    }
+
+     public static void main(String[] args) {
+
+        LinkedListStack stack = new LinkedListStack();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println(stack.peek()); // 30
+
+        System.out.println(stack.pop()); // 30
+        System.out.println(stack.pop()); // 20
+
+        System.out.println(stack.size()); // 1
+    }
+
+}
