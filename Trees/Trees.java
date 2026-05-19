8dbd7406083ef5233e12e68f72e9153c3320c401
Prasadkhose09 <Prasadkhose512@gmail.com>
2026-05-19 07:19:42 +0530

    feat- Post order traversal

--- Trees/Trees.java
+++ Trees/Trees.java
@@ -47,6 +47,14 @@ public class Trees {
         inOrder(root.right);
     }
 
+    public static void postOrder(Node root){
+        if(root == null) return;
+
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data+ " ");
+    }
+
 
     public static void main(String[] args) {
         int[] nodes= {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
