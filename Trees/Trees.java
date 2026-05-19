be5a46c5d503fa220c03c8d08cc6dab89f28afe3
Prasadkhose09 <Prasadkhose512@gmail.com>
2026-05-19 07:19:19 +0530

    feat- in order traversal

--- Trees/Trees.java
+++ Trees/Trees.java
@@ -38,13 +38,27 @@ public class Trees {
 
     }
 
+    public static void inOrder(Node root){
+        if(root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data+ " ");
+        inOrder(root.right);
+    }
+
+
     public static void main(String[] args) {
         int[] nodes= {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
 
-        preOrder(root);
+        System.out.println("Preorder Traversal: "); preOrder(root);
+        System.out.println();
+        System.out.println("InOrder Traversal: ");inOrder(root);
+        System.out.println();
+        System.out.println("PostOrder Traversal: "); postOrder(root);
     }
 
 }
